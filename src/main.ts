e54ca95640cae24485dca0efc1d3b29519db95da
mrnaveira <47919901+mrnaveira@users.noreply.github.com>
2025-03-21 11:46:36 +0000

    documented the safe order presignature issues

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import Safe, { SigningMethod } from '@safe-global/protocol-kit'
+import Safe, { SafeTransactionOptionalProps, SigningMethod } from '@safe-global/protocol-kit'
 import SafeApiKit from '@safe-global/api-kit'
 import { OperationType, SafeTransactionDataPartial } from '@safe-global/types-kit'
 import { encodeFunctionData } from 'viem'
@@ -71,15 +71,17 @@ async function main() {
   console.log('    - Order created, id: ', orderId);
 
   console.log('\nSigning the order')
+  // FIXME: Ideally we want the signing to be automated with the "setOrderPresignature" function call below,
+  // but the safe sdk returns errors on transaction creation and execution.
+  // Example of a similar working manual transaction: 0x9008D19f58AAbD9eD0D60971565AA8510560ab41
+  // await setOrderPresignature(config, sdk, safe, orderId);
+
   console.log('The signing must be done manually from the Safe UI:')
   console.log('   - "New Transaction" -> "Transaction Builder"')
   console.log('   - Enter the COW_SETTLEMENT_CONTRACT address: ', config.COW_SETTLEMENT_CONTRACT,)
   console.log('   - Select the "setPresignature" method')
   console.log('   - Put the order uid: ', orderId)
   console.log('   - Execute with 100k gas limit. Execution on sepolia is flaky (404 errors), must retry a few times')
-  // FIXME: Ideally we want the signing to be automated with the "signOrder" function below,
-  // but sepolia returns Status: 429, {"code":-32005,"message":"rate limit exceeded"}
-  // await signOrder(sdk, safe, orderId);
 }
 
 async function setupSafe() {
@@ -192,7 +194,6 @@ async function buildRepayTransaction(safe: Safe, nonce: number): Promise<string>
     ]
   });
   const repaySafeTxData: SafeTransactionDataPartial = {
-    //to: config.SAFE_ADDRESS,
     to: config.AAVE_POOL_ADDRESS as string,
     value: '0',
     data: repayTxData,
@@ -262,24 +263,42 @@ async function buildWithdrawTransaction(safe: Safe, nonce: number): Promise<stri
   return encodedSafeTransaction;
 }
 
-async function signOrder(sdk: TradingSdk, safe: Safe, orderId: string) {
+async function setOrderPresignature(config: any, sdk: TradingSdk, safe: Safe, orderId: string) {
   const preSignTransaction = await sdk.getPreSignTransaction({
     orderId,
     account: config.SAFE_ADDRESS as string,
   });
+
   const safeTransactionData: SafeTransactionDataPartial = {
-    to: preSignTransaction.to,
+    to: config.COW_SETTLEMENT_CONTRACT as string,
     value: preSignTransaction.value,
     data: preSignTransaction.data,
     operation: OperationType.Call
   }
-  const safeTransaction = await safe.createTransaction({ transactions: [safeTransactionData] });
-  const signedSafeTransaction = await safe.signTransaction(safeTransaction, SigningMethod.ETH_SIGN);
 
+  // These values work in the Safe UI (Transaction Builder) for the same type of transaction
   const options = {
-    gasLimit: 1000000
-  };
+    // FIXME: not sure which of the two gas options (safeTxGas, gas) use here
+    safeTxGas: '200000',
+    gas: 200000n,
+    maxFeePerGas: 241000000n, // 0.26601 Gwei) 
+    maxPriorityFeePerGas: 1000000n // 0.001 Gwei
+  } as SafeTransactionOptionalProps;
 
+  /*
+    FIXME: We are setting the gas limit manually ("gas" option) but the "createTransaction" call below logs an error::
+      Error: cannot estimate gas; transaction may fail or may require manual gas limit [ See: https://links.ethers.org/v5-errors-UNPREDICTABLE_GAS_LIMIT ] (reason="execution reverted", method="estimateGas", transaction={"from":"0x35eD9A9D1122A1544e031Cc92fCC7eA599e28D9C","to":"0x35eD9A9D1122A1544e031Cc92fCC7eA599e28D9C","data":"0xec6cb13f000000000000000000000000000000000000000000000000000000000000004000000000000000000000000000000000000000000000000000000000000000010000000000000000000000000000000000000000000000000000000000000038a83f594602e797a9579096cd92deb6745829cf6a7ae407d80155f86af30b3b4835ed9a9d1122a1544e031cc92fcc7ea599e28d9c67dd36fe0000000000000000","accessList":null}, error={"reason":"processing response error","code":"SERVER_ERROR","body":"{\"id\":44,\"jsonrpc\":\"2.0\",\"error\":{\"code\":3,\"message\":\"execution reverted\"}}","error":{"code":3},"requestBody":"{\"method\":\"eth_estimateGas\",\"params\":[{\"from\":\"0x35ed9a9d1122a1544e031cc92fcc7ea599e28d9c\",\"to\":\"0x35ed9a9d1122a1544e031cc92fcc7ea599e28d9c\",\"data\":\"0xec6cb13f000000000000000000000000000000000000000000000000000000000000004000000000000000000000000000000000000000000000000000000000000000010000000000000000000000000000000000000000000000000000000000000038a83f594602e797a9579096cd92deb6745829cf6a7ae407d80155f86af30b3b4835ed9a9d1122a1544e031cc92fcc7ea599e28d9c67dd36fe0000000000000000\"}],\"id\":44,\"jsonrpc\":\"2.0\"}","requestMethod":"POST","url":"https://sepolia.gateway.tenderly.co"}, code=UNPREDICTABLE_GAS_LIMIT, version=providers/5.8.0)
+  */
+  const safeTransaction = await safe.createTransaction({ transactions: [safeTransactionData], options });
+
+  const signedSafeTransaction = await safe.signTransaction(safeTransaction, SigningMethod.ETH_SIGN);
+  
+  /*
+    FIXME: Execution fails with:
+      Version: viem@2.23.11 cause: HttpRequestError: HTTP request failed.
+      Status: 429 URL: https://sepolia.gateway.tenderly.co Request body: {"method":"eth_sendRawTransaction","params"....
+      {"code":-32005,"message":"rate limit exceeded"}
+  */
   const transactionResult = await safe.executeTransaction(signedSafeTransaction, options);
   console.log('    - Presigned transaction hash: ' + transactionResult.hash);
 }
